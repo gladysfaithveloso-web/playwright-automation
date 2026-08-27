b52e735a8da114691740d2b696f07cf2eba61281
Gladys <gladys.bposeats@gmail.com>
2026-08-28 06:41:19 +0800

    Updated playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,8 +3,13 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
 
+  timeout: 230000,
+
   use: {
-    channel: 'chrome',
-    headless: false,
+    headless: true,
+    screenshot: 'only-on-failure',
+    trace: 'on-first-retry',
   },
+
+  workers: 1,
 });
